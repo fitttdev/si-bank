3faee98891c6365408a2dff9fdd7eb478a40ec44
unimafy <nima.yonten1729@gmail.com>
2023-08-25 17:15:45 +0600

    add unit test

--- db/sqlc/accounts/account_test.go
+++ db/sqlc/accounts/account_test.go
@@ -2,21 +2,21 @@ package accounts
 
 import (
 	"context"
+	"database/sql"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/require"
 )
 
-
-func TestCreateAccount(t *testing.T) {
+func createAccountFactory(t *testing.T) Account {
 	args := CreateAccountParams{
 		Owner: faker.Name(),
 		Balance: rand.Float64(),
 		Currency: faker.Currency(),
 	}
-
 	account, err := testQueries.CreateAccount(context.Background(), args)
 
 	require.NoError(t, err)
@@ -28,4 +28,78 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createAccountFactory(t)
+}
+
+
+func TestGetAccount(t *testing.T) {
+	account1 := createAccountFactory(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account1)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createAccountFactory(t)
+
+	args := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: rand.Float64(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, args.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createAccountFactory(t)
+
+  err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
+
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createAccountFactory(t)
+	}
+
+	args := ListAccountsParams{
+		Limit: 5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), args)
+
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
