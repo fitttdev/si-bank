c0ddd142463c12e0b2482874533e5243f72e63eb
unimafy <nima.yonten1729@gmail.com>
2023-09-08 17:04:46 +0600

    add all the unit test for entry

--- db/sqlc/si_bank_test.go
+++ db/sqlc/si_bank_test.go
@@ -121,6 +121,9 @@ func createEntryFactory(t *testing.T) Entry {
 	return entry
 }
 
+func TestCreateEntry(t *testing.T) {
+	createEntryFactory(t)
+}
 func TestUpdateEntry(t *testing.T) {
 	entry1 := createEntryFactory(t)
 
@@ -137,4 +140,48 @@ func TestUpdateEntry(t *testing.T) {
 	require.NotEqual(t, entry1.Amount, entry2.Amount)
 	require.Equal(t, args.Amount, entry2.Amount)
 	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createEntryFactory(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry1)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	entry1 := createEntryFactory(t)
+
+	err := testQueries.DeleteEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createEntryFactory(t)
+	}
+
+	args := ListEntriesParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), args)
+
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
 }
